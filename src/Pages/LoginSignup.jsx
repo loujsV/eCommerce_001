895bd71f028fad79ecba1ad4a40cb2d1ddbae768
Louis Long Bui <long.bui.jen@gmail.com>
2024-07-03 13:32:00 -0700

    finish

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -17,9 +17,43 @@ const LoginSignup = () => {
 
   const login = async() => {
     console.log("Login", formData)
+    let responseData;
+    await fetch('http://localhost:4000/login', {
+      method: 'POST',
+      headers:{
+        Accept:'application/json', 
+        'Content-Type':'application/json'},
+      body: JSON.stringify(formData)
+    }).then((response) => response.json())
+     .then((data) => responseData = data)
+
+     if(responseData.success){
+        localStorage.setItem('auth-token', responseData.token);
+        window.location.replace('/');
+      }
+     else{
+        alert(responseData.errors);
+      }
   }
   const signup = async() => {
-    console.log("Signup", formData)
+    console.log("Signup", formData);
+    let responseData;
+    await fetch('http://localhost:4000/signup', {
+      method: 'POST',
+      headers:{
+        Accept:'application/json', 
+        'Content-Type':'application/json'},
+      body: JSON.stringify(formData)
+    }).then((response) => response.json())
+     .then((data) => responseData = data)
+
+     if(responseData.success){
+        localStorage.setItem('auth-token', responseData.token);
+        window.location.replace('/');
+      }
+     else{
+        alert(responseData.errors);
+      }
   }
   return (
     <div className='loginsignup'>
